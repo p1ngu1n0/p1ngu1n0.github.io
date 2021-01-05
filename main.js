76ae38062558f18c18d5b3e6fa4679226920c876
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2021-01-05 18:57:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var encrypted = CryptoJS.AES.encrypt("spammimic", "007");
-
 function Main() {
     var encrypted = CryptoJS.AES.decrypt("U2FsdGVkX18z+MqmrzMIbIXppYg2K97iu+YJgi51GvPR14KT3FakkA7jUvBq5SyrvtkyaiFLn+sKYym/QN3VTpEAAF8Q4IlGvWWOh0TiNrY=", document.getElementById("passwd").value);
     document.getElementById("res").innerHTML = encrypted.toString(CryptoJS.enc.Utf8); 
