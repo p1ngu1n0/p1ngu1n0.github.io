98637e2b629d903b30985970b25b9193597f928e
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2021-01-05 18:52:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-//var encrypted = CryptoJS.AES.encrypt();
+var encrypted = CryptoJS.AES.encrypt("spammimic", "007");
 
 function Main() {
     var encrypted = CryptoJS.AES.decrypt("U2FsdGVkX18z+MqmrzMIbIXppYg2K97iu+YJgi51GvPR14KT3FakkA7jUvBq5SyrvtkyaiFLn+sKYym/QN3VTpEAAF8Q4IlGvWWOh0TiNrY=", document.getElementById("passwd").value);
     document.getElementById("res").innerHTML = encrypted.toString(CryptoJS.enc.Utf8); 
     document.getElementById("res").href = encrypted.toString(CryptoJS.enc.Utf8); 
 };
+
+function pista() {console.log(CryptoJS.AES.decrypt("U2FsdGVkX18eKoy0pnpthLR7seZOwfquiKPxgZjc7Ho=", "007").toString(CryptoJS.enc.Utf8))};
 
